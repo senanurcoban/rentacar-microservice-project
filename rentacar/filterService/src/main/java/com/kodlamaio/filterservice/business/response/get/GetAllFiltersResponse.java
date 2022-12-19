b90eb2942832c72159c5b01a9793c8a29eb473b2
Seda Coban <scoban@ankarabt.com>
2022-12-19 16:07:55 +0300

    rentacar microservice project

--- rentacar/filterService/src/main/java/com/kodlamaio/filterservice/business/response/get/GetAllFiltersResponse.java
+++ rentacar/filterService/src/main/java/com/kodlamaio/filterservice/business/response/get/GetAllFiltersResponse.java
@@ -1,5 +1,31 @@
 package com.kodlamaio.filterservice.business.response.get;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetAllFiltersResponse {
 
+    private String id;
+	
+	private String carId;
+	
+	private String brandId;
+
+	private String brandName;
+
+	private String modelId;
+
+	private String modelName;
+
+	private double dailyPrice;
+
+	private int modelYear;
+
+	private String plate;
+
+	private int state;
 }
--- rentacar/filterService/src/main/java/com/kodlamaio/filterservice/business/response/get/GetFilterResponse.java
+++ rentacar/filterService/src/main/java/com/kodlamaio/filterservice/business/response/get/GetFilterResponse.java
@@ -1,5 +1,30 @@
 package com.kodlamaio.filterservice.business.response.get;
 
-public class GetFilterResponse {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetFilterResponse {
+	private String id;
+
+	private String carId;
+
+	private String brandId;
+
+	private String brandName;
+
+	private String modelId;
+
+	private String modelName;
+
+	private double dailyPrice;
+
+	private int modelYear;
+
+	private String plate;
+
+	private int state;
 }
